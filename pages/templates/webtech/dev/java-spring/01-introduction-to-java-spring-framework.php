2fed654ab280e4f56e28902f827e7826b79d277e
Anup Chakravarthi <nellutlalnrao@gmail.com>
2020-08-17 21:12:55 +0530

    Updated Pages

--- pages/templates/webtech/dev/java-spring/01-introduction-to-java-spring-framework.php
+++ pages/templates/webtech/dev/java-spring/01-introduction-to-java-spring-framework.php
@@ -6,15 +6,22 @@ ReactDOM.render(<ContentMenu data={pageContent}
 </script>
 <div class="row">
 <div class="col-sm-7 mbot35p">
-<div><h1 class="font-red"><b>Modules of Spring Framework</b></h1></div>
+<div align="center"><h1 class="font-red"><b>Modules of Spring Framework</b></h1></div>
 <div class="row">
 <div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Core</b></div></div></div>
-<div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Boot</b></div></div></div>
+<div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring MVC</b></div></div></div>
 <div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring AOP</b></div></div></div>
+<div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Boot</b></div></div></div>
 <div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Data</b></div></div></div>
 <div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Session</b></div></div></div>
 <div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Security</b></div></div></div>
 </div><!--/.row -->
+
+<div><h1><b>Spring Core</b></h1></div>
+<div>It is the core and the backbone component of the Spring framework that provides 
+<code>IoC Container</code>, 
+<code>Spring Dependency Injection</code>, <code>Spring Bean and its Scope</code> and 
+<code>Container Configuration</code>.</div>
 
 </div><!--/.col-sm-7 -->
 <div class="col-sm-5">
