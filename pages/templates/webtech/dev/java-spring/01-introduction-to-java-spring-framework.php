853c3947b71d07f538e4caddfafac7de87c0978e
Anup Chakravarthi <nellutlalnrao@gmail.com>
2020-08-17 20:50:24 +0530

    Updated Pages

--- pages/templates/webtech/dev/java-spring/01-introduction-to-java-spring-framework.php
+++ pages/templates/webtech/dev/java-spring/01-introduction-to-java-spring-framework.php
@@ -6,9 +6,23 @@ ReactDOM.render(<ContentMenu data={pageContent}
 </script>
 <div class="row">
 <div class="col-sm-7 mbot35p">
+<div><h1 class="font-red"><b>Modules of Spring Framework</b></h1></div>
+<div class="row">
+<div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Core</b></div></div></div>
+<div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Boot</b></div></div></div>
+<div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring AOP</b></div></div></div>
+<div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Data</b></div></div></div>
+<div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Session</b></div></div></div>
+<div class="col-sm-4"><div class="list-group"><div align="center" class="list-group-item"><b>Spring Security</b></div></div></div>
+</div><!--/.row -->
 
-</div>
+</div><!--/.col-sm-7 -->
 <div class="col-sm-5">
-
-</div>
-</div>
+<div class="list-group">
+<div class="list-group-item">
+<div><h5><b>Spring Framework Test Support</b></h5></div>
+<div>Spring Framework supports the JUNIT and TestNG Testing Frameworks.</div>
+</div><!--/.list-group-item -->
+</div><!--/.list-group -->
+</div><!--/.col-sm-5 -->
+</div><!--/.row -->
