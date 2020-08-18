75ebd5fbc5d622da7c62755364f2e295b2b77bf5
Anup Chakravarthi <nellutlalnrao@gmail.com>
2020-08-19 01:16:00 +0530

    Updated pages

--- pages/templates/webtech/dev/java/02-java-interfaces-comparable-and-comparator.php
+++ pages/templates/webtech/dev/java/02-java-interfaces-comparable-and-comparator.php
@@ -8,7 +8,7 @@ ReactDOM.render(<ContentMenu data={pageContent}
 <div class="col-sm-7 mbot35p">
   <div><h1 class="font-red"><b>Java Interfaces - Comparable and Comparator</b></h1></div>
   <div>
-  
+  Comparable and Comparator are used to sort the Collection Elements.
   </div>
   <!-- -->
   <pre class="code">
@@ -16,6 +16,47 @@ ReactDOM.render(<ContentMenu data={pageContent}
   
   </code>
   </pre>
+  <!-- -->
+  <!-- -->
+  <div><h5><b>See the main differences between Comparable and Comparator Interfaces:</b></h5></div>
+  <div class="table-responsive">          
+  <table class="table">
+    <thead>
+      <tr class="bg-darkPurple">
+        <th>S.No.</th>
+        <th>Comparable</th>
+        <th>Comparator</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>It is present in <code>java.lang</code> package.</td>
+        <td>It is present in <code>java.util</code> package.</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>
+          <div><b>Single Sorting Sequence</b></div>
+          <div>It is able to sort the Collection with a Single Element</div>
+        </td>
+        <td>
+          <div><b>Multiple Sorting Sequence</b></div>
+          <div>It is able to sort the Collection with a Multiple Elements</div>
+        </td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>
+          Sorting modifies the Actual Class
+        </td>
+        <td>
+          Sorting doesn't modify the Actual Class
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
   <!-- -->
 </div>
 <div class="col-sm-5">
