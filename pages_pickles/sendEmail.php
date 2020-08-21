4aa4e0669f256f230aac1f18f13609636d2b7cba
Anup Chakravarthi <nellutlalnrao@gmail.com>
2020-08-21 16:48:26 +0530

    Updated pages

--- pages_pickles/sendEmail.php
+++ pages_pickles/sendEmail.php
@@ -4,13 +4,16 @@
   $to = "nellutlalnrao@gmail.com";
   $subject = "This is subject";
   $message= "<img src=\"http://widesecond.com/pages_pickles/logo.jpg\"/>";
-  $message .= "<h3><b><i>Thanks for Visiting our Website and choosing to Register an Account, <br/>";
-  $message .= "we have received your request to validate your Email Address for Signup.</i></b></h3><br/>";
-  $message .= "<h1><b>Your OTP Code : 123456</b></h1>";
-  $message .= "<div align=\"center\" style=\"font-size:9px;\">To ensure delivery to your inbox, add ";
+  $message .= "<div style=\"background-color:#027d72;color:#fff;\">";
+  $message .= "<h3 style=\"padding:8px;\"><b><i>Thanks for Visiting our Website and choosing to Register an Account, <br/>";
+  $message .= "we have received your request to validate your Email Address for Signup.</i></b></h3>";
+  $message .= "</div>";
+  $message .= "<h2><b>Your OTP Code : 123456</b></h2>";
+ 
+  $message .= "<div align=\"left\" style=\"font-size:10px;\">To ensure delivery to your inbox, add ";
   $message .= "mail@widesecond.com to your address book.</div>";
   echo $message;   
-         $header = "From:mail@widesecond.com \r\n";
+   /*      $header = "From:mail@widesecond.com \r\n";
          $header .= "Cc:nlnrao228@gmail.com \r\n";
          $header .= "MIME-Version: 1.0\r\n";
          $header .= "Content-type: text/html\r\n";
@@ -21,5 +24,5 @@
             echo "Message sent successfully...";
          }else {
             echo "Message could not be sent...";
-         }
+         }*/
 ?>
